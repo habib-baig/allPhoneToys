db964265cd03f4eaaa05c46f17267962395959bb
Jibin Rajan Varghese <email.jrv@gmail.com>
2017-10-01 01:00:56 -0500

    Sign Up page for review

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
 
   validates :name,  presence: true, length: { maximum: 50 }
+
+  validates :password,  presence: true, length: { maximum: 50 }
+
+  validates :prefAmount,numericality: { only_integer: true, less_than_or_equal_to:1000 }
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
             format: { with: VALID_EMAIL_REGEX }
@@ -9,7 +14,11 @@ class User < ApplicationRecord
   enum prefProvider: { Lyca: 'Lyca_Mobile', TMobile: 'T- Mobile',Verizon: 'Verizon'}
   PREF_PROVIDER = User.prefProviders.map { |k, v| [k.to_s.humanize, v] }
 
+  validates :prefProvider, presence: true
+
   enum prefLocation: { College_Main: 'college-mains', Cherry_Hollows: 'cherry_hollows', Gardens: 'Gardens', Scandia: 'Scandia'}
   PREF_LOCATION = User.prefLocations.map { |k, v| [k.to_s.humanize, v] }
+
+  validates :prefLocation, presence: true
 
 end
