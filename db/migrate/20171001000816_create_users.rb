d28ab8fdab74c0005f37d5bf0c3de3fc07984eb6
Jibin Rajan Varghese <email.jrv@gmail.com>
2017-09-30 19:21:09 -0500

    Initial commit : User table added

---
+++ db/migrate/20171001000816_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.integer :phone, :limit => 8
+      t.string :password
+      t.integer :prefProvider
+      t.integer :prefAmount
+      t.integer :prefLocation
+
+      t.timestamps
+    end
+  end
+end
