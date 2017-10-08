d174aeb442ce2d5add0dbb2f8249d3aac12033c9
Jibin Rajan Varghese <email.jrv@gmail.com>
2017-10-08 14:51:41 -0500

    Testing enum fix

--- db/migrate/20171001000816_create_users.rb
+++ db/migrate/20171001000816_create_users.rb
@@ -5,9 +5,9 @@ class CreateUsers < ActiveRecord::Migration[5.1]
       t.string :email
       t.integer :phone, :limit => 8
       t.string :password
-      t.integer :prefProvider
+      t.string :prefProvider
       t.integer :prefAmount
-      t.integer :prefLocation
+      t.string :prefLocation
       t.timestamps
     end
   end
