44d17981d4463d006668723d75474b0bd553b7c5
Jibin Rajan Varghese <email.jrv@gmail.com>
2017-10-14 17:24:51 -0500

    Edited Routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
   resources :transactions
   get 'sessions/new'
-  get 'transactions/index'
-
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'sessions#new'
-  get  'transactions#index'
+
+  get   '/transactions', to: 'transactions#index'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
