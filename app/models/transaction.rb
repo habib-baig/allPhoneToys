8d9cb86c02dff7ea36eada34c4d6bbc16e462e2b
Vivek Chirumamilla <vivek.chirumamilla@ggktech.com>
2017-10-21 15:51:33 -0500

    Updated schedule pickup functionality

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,6 @@
 class Transaction < ApplicationRecord
+
+  enum providers: { Lyca: 1 , T_My_Mobile: 2 ,Verizon: 3 }
+  enum locations: { College_Main: 1 , Cherry_Hollows: 2 , Gardens: 3 , Scandia: 4 }
+
 end
